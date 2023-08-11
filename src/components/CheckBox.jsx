9b0fcaf1d1322fc994225c105eeb4e53e6af6a66
jjang-aaa <jadoit473@gmail.com>
2023-08-11 19:44:23 +0900

    환경설정 후 테스트

---
+++ src/components/CheckBox.jsx
@@ -0,0 +1,9 @@
+function CheckBox() {
+  return (
+    <button>
+      <img src="./assets/isChecked=false" alt="선택 안됨" />
+    </button>
+  );
+}
+
+export default CheckBox;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
 import './styles/global.css';
+import CheckBox from './components/\bCheckBox';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <h1> 리액트 숙제로 체크박스 만들기 </h1>
+    <CheckBox />
   </React.StrictMode>
 );
